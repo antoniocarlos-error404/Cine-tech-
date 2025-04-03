01e6254543d69bfad7e597f54333cf9c25fe9a1b
antonio <otavioantonio.2003@gmail.com>
2025-04-03 17:22:50 -0300

    botao do header

--- views/header.php
+++ views/header.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CineTech - Filmes</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css"> <!-- Ícones Bootstrap -->
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
@@ -20,11 +21,15 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Filmes</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin.php">Área Administrativa</a>
+                <!-- Botão moderno com dropdown -->
+                <li class="nav-item dropdown">
+                    <button class="btn btn-primary dropdown-toggle px-4 py-2 d-flex align-items-center" type="button" id="navbarDropdown" data-bs-toggle="dropdown">
+                        <i class="bi bi-list me-2"></i> Opções
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="index.php"><i class="bi bi-film me-2"></i> Filmes</a></li>
+                        <li><a class="dropdown-item" href="admin.php"><i class="bi bi-gear me-2"></i> Área Administrativa</a></li>
+                    </ul>
                 </li>
             </ul>
         </div>
